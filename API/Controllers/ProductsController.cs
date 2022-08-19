76bf48e4a9bf393f0b90f399b4475fab12fd1e4b
Mykeal Wheeler <mykeal.wheeler@allata.com>
2022-08-19 15:33:38 -0600

    section 3 commit done

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -29,5 +29,16 @@ namespace API.Controllers
         {
             return await _repo.GetProductByIdAsync(id);
         }
+
+        [HttpGet("brands")]
+        public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetProductBrands()
+        {
+            return Ok(await _repo.GetProductBrandAsync());
+        }
+        [HttpGet("types")]
+        public async Task<ActionResult<IReadOnlyList<ProductType>>> GetProductTypes()
+        {
+            return Ok(await _repo.GetProductTypesAsync());
+        }
     }
 }
