bfc9a02a22c31b618658600fd5d6ad905c446da2
Mykeal Wheeler <mykeal.wheeler@allata.com>
2022-09-07 15:16:36 -0600

    final

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -22,25 +22,22 @@ namespace API.Controllers
     {
         private readonly UserManager<AppUser> _userManager;
         private readonly SignInManager<AppUser> _signInManager;
-        private readonly IMapper _mapper;
-
         private readonly ITokenService _tokenService;
-        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, ITokenService tokenService, IMapper mapper)
+        private readonly IMapper _mapper;
+        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager,
+            ITokenService tokenService, IMapper mapper)
         {
+            _mapper = mapper;
             _tokenService = tokenService;
             _signInManager = signInManager;
             _userManager = userManager;
-            _mapper = mapper;
-
         }
 
-        [HttpGet]
         [Authorize]
+        [HttpGet]
         public async Task<ActionResult<UserDto>> GetCurrentUser()
         {
-            var email = User.FindFirstValue(ClaimTypes.Email);
-
-            var user = await _userManager.FindByEmailAsync(email);
+            var user = await _userManager.FindByEmailFromClaimsPrinciple(User);
 
             return new UserDto
             {
@@ -56,19 +53,17 @@ namespace API.Controllers
             return await _userManager.FindByEmailAsync(email) != null;
         }
 
-        [HttpGet("address")]
         [Authorize]
-        public async Task<ActionResult<Address>> GetUserAddress()
+        [HttpGet("address")]
+        public async Task<ActionResult<AddressDto>> GetUserAddress()
         {
-            var email = User.FindFirstValue(ClaimTypes.Email);
+            var user = await _userManager.FindByEmailWithAddressAsync(User);
 
-            var user = await _userManager.FindByEmailAsync(email);
-
-            return user.Address;
+            return _mapper.Map<AddressDto>(user.Address);
         }
 
-        [HttpPut("address")]
         [Authorize]
+        [HttpPut("address")]
         public async Task<ActionResult<AddressDto>> UpdateUserAddress(AddressDto address)
         {
             var user = await _userManager.FindByEmailWithAddressAsync(User);
@@ -82,21 +77,17 @@ namespace API.Controllers
             return BadRequest("Problem updating the user");
         }
 
+
         [HttpPost("login")]
         public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
         {
             var user = await _userManager.FindByEmailAsync(loginDto.Email);
 
-            if (user == null)
-            {
-                return Unauthorized(new ApiResponse(401));
-            }
+            if (user == null) return Unauthorized(new ApiResponse(401));
 
             var result = await _signInManager.CheckPasswordSignInAsync(user, loginDto.Password, false);
-            if (!result.Succeeded)
-            {
-                return Unauthorized(new ApiResponse(401));
-            }
+
+            if (!result.Succeeded) return Unauthorized(new ApiResponse(401));
 
             return new UserDto
             {
@@ -118,10 +109,7 @@ namespace API.Controllers
 
             var result = await _userManager.CreateAsync(user, registerDto.Password);
 
-            if (!result.Succeeded)
-            {
-                return BadRequest(new ApiResponse(400));
-            }
+            if (!result.Succeeded) return BadRequest(new ApiResponse(400));
 
             return new UserDto
             {
@@ -130,8 +118,5 @@ namespace API.Controllers
                 Email = user.Email
             };
         }
-
-
-
     }
 }
